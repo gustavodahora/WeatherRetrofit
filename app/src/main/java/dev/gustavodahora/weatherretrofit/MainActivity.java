b8a7f557a7f881753137f56c64537c4545992869
Gustavo da Hora <gustavo.carmodahora@outlook.com>
2022-03-04 00:00:39 -0300

    Adding new views on code controller.

--- app/src/main/java/dev/gustavodahora/weatherretrofit/MainActivity.java
+++ app/src/main/java/dev/gustavodahora/weatherretrofit/MainActivity.java
@@ -3,12 +3,27 @@ package dev.gustavodahora.weatherretrofit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvCity;
+    TextView tvTempActual;
+    TextView tvTempMaximum;
+    TextView tvTempMinimum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupViews();
+    }
+
+    public void setupViews() {
+        tvCity = findViewById(R.id.tv_city);
+        tvTempActual = findViewById(R.id.tv_temp_actual);
+        tvTempMaximum = findViewById(R.id.tv_temp_max);
+        tvTempMinimum = findViewById(R.id.tv_temp_min);
     }
 }
