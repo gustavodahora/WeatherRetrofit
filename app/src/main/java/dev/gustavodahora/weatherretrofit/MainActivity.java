98e13d574a42625925f031d6f438259b3c9e531f
Gustavo da Hora <gustavo.carmodahora@outlook.com>
2022-03-05 03:43:48 -0300

    Adding new system to control weather.

--- app/src/main/java/dev/gustavodahora/weatherretrofit/MainActivity.java
+++ app/src/main/java/dev/gustavodahora/weatherretrofit/MainActivity.java
@@ -4,6 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import dev.gustavodahora.weatherretrofit.model.WeatherData;
+import dev.gustavodahora.weatherretrofit.retroint.APIWeatherCall;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,12 +21,15 @@ public class MainActivity extends AppCompatActivity {
     TextView tvTempMaximum;
     TextView tvTempMinimum;
 
+    String BASE_URL = "https://api.openweathermap.org/data/2.5/";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         setupViews();
+        callApi();
     }
 
     public void setupViews() {
@@ -25,5 +37,42 @@ public class MainActivity extends AppCompatActivity {
         tvTempActual = findViewById(R.id.tv_temp_actual);
         tvTempMaximum = findViewById(R.id.tv_temp_max);
         tvTempMinimum = findViewById(R.id.tv_temp_min);
+    }
+
+    public void callApi() {
+        try {
+            // Retrofit Builder
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+            // instance for interface
+            APIWeatherCall apiWeatherCall = retrofit.create(APIWeatherCall.class);
+
+            Call<WeatherData> call = apiWeatherCall.getWeatherData();
+
+            call.enqueue(new Callback<WeatherData>() {
+                @Override
+                public void onResponse(Call<WeatherData> call, Response<WeatherData> response) {
+                    // Checking for the response
+                    if (response.code() != 200) {
+                        Toast.makeText(MainActivity.this, getString(R.string.error_api_call), Toast.LENGTH_LONG).show();
+                        return;
+                    }
+
+                    tvTempActual.setText(response.body().getMain().getTemp().toString());
+                    tvTempMaximum.setText(response.body().getMain().getTempMax().toString());
+                    tvTempMinimum.setText(response.body().getMain().getTempMin().toString());
+                }
+
+                @Override
+                public void onFailure(Call<WeatherData> call, Throwable t) {
+                    Toast.makeText(MainActivity.this, "Error = " + t, Toast.LENGTH_LONG).show();
+                }
+            });
+        } catch (Exception e) {
+            Toast.makeText(MainActivity.this, "Error = " + e, Toast.LENGTH_LONG).show();
+        }
     }
 }
---
+++ app/src/main/java/dev/gustavodahora/weatherretrofit/retroint/APIWeatherCall.java
@@ -0,0 +1,15 @@
+package dev.gustavodahora.weatherretrofit.retroint;
+
+import dev.gustavodahora.weatherretrofit.model.WeatherData;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface APIWeatherCall {
+    // https://run.mocky.io/v3/
+    // a8385ac6-c51f-4cdd-9a41-7e3f2eb77a9c
+
+    String API_KEY = "97c5571d9fc1e803990872a56dbe037f";
+
+    @GET("weather?lat=35&lon=139&appid=97c5571d9fc1e803990872a56dbe037f")
+    Call<WeatherData> getWeatherData();
+}
